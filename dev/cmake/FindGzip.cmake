93f7cf19f7f3c7f982f37b3c371185a29e4c290c
J. Bromley <jay@appliedinvention.com>
2024-12-17 17:05:59 -0800

    Fixed warning caused by incorrect name in FindGzip.cmake
    The file capitalizing Gzip in a way inconsistent with the find_package
    statement in the top-level CMakeLists.txt. Fixed and eliminated the
    warning.

--- dev/cmake/FindGzip.cmake
+++ dev/cmake/FindGzip.cmake
@@ -6,15 +6,15 @@
 find_program(GZIP_EXE gzip)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( gzip DEFAULT_MSG GZIP_EXE )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Gzip DEFAULT_MSG GZIP_EXE )
 mark_as_advanced(GZIP_EXE)
 
 macro(gzip_compress target_name input_files result_archive)
-    add_custom_command(
+  add_custom_command(
         OUTPUT ${result_archive}
         COMMAND ${GZIP_EXE} --best --to-stdout ${input_files} > ${result_archive}
         COMMENT "Compressing ${input_files}"
         DEPENDS ${input_files} ${ARGN}
     )
-    add_custom_target(${target_name} ALL DEPENDS ${result_archive})
+  add_custom_target(${target_name} ALL DEPENDS ${result_archive})
 endmacro()
